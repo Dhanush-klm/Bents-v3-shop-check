9b42c65ae8bfae44e49ea334d9917c3f977c1945
Dhanush-klm <dhanush@klmsolutions.in>
2025-02-21 17:12:11 +0530

    netlify files

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  target: process.env.NEXT_PRIVATE_TARGET === 'server' ? 'server' : 'experimental-serverless-trace',
+  generateBuildId: async () => {
+    return 'build-' + Date.now();
+  }
 };
 
 export default nextConfig;
