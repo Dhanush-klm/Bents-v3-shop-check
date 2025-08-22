01531ac2ef380f13c5f3492124b4d5a3428c50a1
dhanush-klm <dhanush@klmsolutions.in>
2025-08-23 00:19:57 +0530

    eslint updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["app/emails/**/*.tsx"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
